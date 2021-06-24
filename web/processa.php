6d9f28e67a1d44254d730f7dbbefea9ab8ddc474
FLS <alan7@outlook.com>
2021-06-24 20:48:02 -0300

    add ids..

--- web/processa.php
+++ web/processa.php
@@ -15,16 +15,18 @@ $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_NUMBER_INT);
 $complemento = filter_input(INPUT_POST, 'complemento', FILTER_SANITIZE_NUMBER_INT);
 $bairro = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_STRING);
 $aluguel = filter_input(INPUT_POST, 'aluguel', FILTER_SANITIZE_NUMBER_INT);
-$data = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_STRING);
-$hora = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_STRING);
-$mensagem = filter_input(INPUT_POST, 'salas2', FILTER_SANITIZE_STRING);
+$data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
+$hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
+$mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
 
-$salas2 = filter_input(INPUT_POST, 'salas2', FILTER_SANITIZE_STRING);
-$andar = filter_input(INPUT_POST, 'area', FILTER_SANITIZE_NUMBER_INT);
-$condominio = filter_input(INPUT_POST, 'area', FILTER_SANITIZE_NUMBER_INT);
-$portaria = filter_input(INPUT_POST, 'salas2', FILTER_SANITIZE_STRING);
-
-$salas = filter_input(INPUT_POST, 'area', FILTER_SANITIZE_NUMBER_INT);
+if($tipoImovel == 'Apartamento'){
+	$salas2 = filter_input(INPUT_POST, 'salas2', FILTER_SANITIZE_STRING);
+	$andar = filter_input(INPUT_POST, 'andar', FILTER_SANITIZE_NUMBER_INT);
+	$condominio = filter_input(INPUT_POST, 'condominio', FILTER_SANITIZE_NUMBER_INT);
+	$portaria = filter_input(INPUT_POST, 'portaria', FILTER_SANITIZE_STRING);
+}else if($tipoImovel == 'Casa'){
+	$salas = filter_input(INPUT_POST, 'salas', FILTER_SANITIZE_NUMBER_INT);
+}
 
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
