d613bcd21d4ebadf22664b2c1f485759234bed9b
FLS <alan7@outlook.com>
2021-06-24 20:56:45 -0300

    insert ok

--- web/processa.php
+++ web/processa.php
@@ -24,14 +24,16 @@ if($tipoImovel == 'Apartamento'){
 	$andar = filter_input(INPUT_POST, 'andar', FILTER_SANITIZE_NUMBER_INT);
 	$condominio = filter_input(INPUT_POST, 'condominio', FILTER_SANITIZE_NUMBER_INT);
 	$portaria = filter_input(INPUT_POST, 'portaria', FILTER_SANITIZE_STRING);
+	$result_usuario = "INSERT INTO imobiliaria (tipoImovel,quartos, suites, vagas, area,armario, rua, numero, complemento,bairro, aluguel, data, hora, mensagem,salas2,andar,condominio,portaria) VALUES ('$tipoImovel','$quartos', '$suites','$vagas', '$area','$armario','$rua','$numero','$complemento','$bairro','$aluguel','$data','$hora','$mensagem','$salas2','$andar','$condominio','$portaria')";
 }else if($tipoImovel == 'Casa'){
 	$salas = filter_input(INPUT_POST, 'salas', FILTER_SANITIZE_NUMBER_INT);
+	$result_usuario = "INSERT INTO imobiliaria (tipoImovel,quartos, suites, vagas, area,armario, rua, numero, complemento,bairro, aluguel, data, hora, mensagem,salas2,andar,condominio,portaria) VALUES ('$tipoImovel','$quartos', '$suites','$vagas', '$area','$armario','$rua','$numero','$complemento','$bairro','$aluguel','$data','$hora','$mensagem','$salas')";
 }
 
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
 
-$result_usuario = "INSERT INTO imobiliaria (quartos, suites, vagas, area,tipoImovel,armario, rua, numero, complemento,bairro, aluguel) VALUES ('$quartos', '$suites','$vagas', '$area','$tipoImovel','$armario','$rua','$numero','$complemento','$bairro','$aluguel')";
+
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if(mysqli_insert_id($conn)){
