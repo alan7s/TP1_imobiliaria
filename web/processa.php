229075a04fc80f18b8028add017ab8035bdf4255
FLS <alan7@outlook.com>
2021-06-24 23:03:06 -0300

    fix bd

--- web/processa.php
+++ web/processa.php
@@ -2,7 +2,7 @@
 session_start();
 include_once("conexao.php");
 
-$tipoImovel = filter_input(INPUT_POST, 'tipoImovel', FILTER_SANITIZE_STRING);
+$tipoimovel = filter_input(INPUT_POST, 'tipoimovel', FILTER_SANITIZE_STRING);
 
 $quartos = filter_input(INPUT_POST, 'quartos', FILTER_SANITIZE_NUMBER_INT);
 $suites = filter_input(INPUT_POST, 'suites', FILTER_SANITIZE_NUMBER_INT);
@@ -12,23 +12,24 @@ $armario = filter_input(INPUT_POST, 'armario', FILTER_SANITIZE_STRING);
 
 $rua = filter_input(INPUT_POST, 'rua', FILTER_SANITIZE_STRING);
 $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_NUMBER_INT);
-$complemento = filter_input(INPUT_POST, 'complemento', FILTER_SANITIZE_NUMBER_INT);
+$complemento = filter_input(INPUT_POST, 'complemento', FILTER_SANITIZE_STRING);
 $bairro = filter_input(INPUT_POST, 'bairro', FILTER_SANITIZE_STRING);
 $aluguel = filter_input(INPUT_POST, 'aluguel', FILTER_SANITIZE_NUMBER_INT);
 $data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING);
 $hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
 $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
 
-if($tipoImovel == 'Apartamento'){
-	$salas2 = filter_input(INPUT_POST, 'salas2', FILTER_SANITIZE_STRING);
+//if($tipoImovel == 'Apartamento'){
+	$salasjanter = filter_input(INPUT_POST, 'salasjantar', FILTER_SANITIZE_NUMBER_INT);
+	$salasestar = filter_input(INPUT_POST, 'salasestar', FILTER_SANITIZE_NUMBER_INT);
 	$andar = filter_input(INPUT_POST, 'andar', FILTER_SANITIZE_NUMBER_INT);
 	$condominio = filter_input(INPUT_POST, 'condominio', FILTER_SANITIZE_NUMBER_INT);
 	$portaria = filter_input(INPUT_POST, 'portaria', FILTER_SANITIZE_STRING);
-	$result_usuario = "INSERT INTO imobiliariaAP (tipoImovel,quartos, suites, vagas, area,armario, rua, numero, complemento,bairro, aluguel, data, hora, mensagem,salas2,andar,condominio,portaria) VALUES ('$tipoImovel','$quartos', '$suites','$vagas', '$area','$armario','$rua','$numero','$complemento','$bairro','$aluguel','$data','$hora','$mensagem','$salas2','$andar','$condominio','$portaria')";
-}else if($tipoImovel == 'Casa'){
+	$result_usuario = "INSERT INTO imobiliariaAP (tipoimovel,quartos, suites, vagas, area,armario, rua, numero, complemento,bairro, aluguel, data, hora, mensagem,salas,andar,condominio,portaria) VALUES ('$tipoimovel','$quartos', '$suites','$vagas', '$area','$armario','$rua','$numero','$complemento','$bairro','$aluguel','$data','$hora','$mensagem','$salasjantar','$salasestar','$andar','$condominio','$portaria')";
+/*}else if($tipoImovel == 'Casa'){
 	$salas = filter_input(INPUT_POST, 'salas', FILTER_SANITIZE_NUMBER_INT);
 	$result_usuario = "INSERT INTO imobiliaria (tipoImovel,quartos, suites, vagas, area,armario, rua, numero, complemento,bairro, aluguel, data, hora, mensagem,salas2,andar,condominio,portaria) VALUES ('$tipoImovel','$quartos', '$suites','$vagas', '$area','$armario','$rua','$numero','$complemento','$bairro','$aluguel','$data','$hora','$mensagem','$salas')";
-}
+}*/
 
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
